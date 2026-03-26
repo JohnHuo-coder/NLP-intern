cce50cf6fa7f0a16f9d2192b924786f9cc6adea6
Jingyu Huo <huojingyu7@gmail.com>
2026-03-26 16:00:22 -0500

    modified data loading scripts to support loading unique cities and numeric columns to df

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import pandas as pd
+import json
 
 conn = mysql.connector.connect(
     host = 'localhost', user = 'root', password = 'root', database='real_estate'
@@ -14,4 +15,27 @@ ORDER BY RAND() LIMIT 1000
 """
 df = pd.read_sql(query, conn)
 df.to_csv('data/processed/listing_sample.csv', index = False)
+
+extract_distinct_query = """
+SELECT DISTINCT L_City 
+FROM rets_property
+WHERE L_City IS NOT NULL AND L_City != ''
+"""
+df_city = pd.read_sql(extract_distinct_query, conn)
+df_city.to_csv('data/processed/distinct_cities.csv', index = False)
+
+load_numeric_value_query = """
+SELECT 
+    L_Keyword2 as bedrooms,
+    LM_Dec_3 as bathrooms,
+    L_SystemPrice as price,
+    L_Keyword1 as sqft
+FROM rets_property;   
+"""
+# keyword 1 is lot size area in sqft, use LM_Int2_3 for living area
+
+df_numeric = pd.read_sql(load_numeric_value_query, conn)
+df_numeric.to_csv('data/processed/numeric_values.csv', index = False)
+
 conn.close
+
